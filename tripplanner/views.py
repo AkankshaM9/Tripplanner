5480aa236594d682b3a91ce7e24dc97332d4a433
Akshata Badola <akshatabadola@gmail.com>
2023-10-18 23:38:35 +0530

    .

--- tripplanner/views.py
+++ tripplanner/views.py
@@ -3,7 +3,7 @@ from trips.models import *
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login, logout
-
+from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from trips.forms import *
 
