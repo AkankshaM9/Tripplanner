e294890e4c6956fef2f578963c92f9820faedfa6
akanksha <akanksha9.m@gmail.com>
2023-10-18 12:48:37 +0530

    added models

--- trips/models.py
+++ trips/models.py
@@ -3,10 +3,22 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 
-
 class User(AbstractUser): # Subclassing Django's User model for customization
     dob = models.DateField()
 
+class Location(models.Model):
+    name = models.CharField(max_length=45)
+    state = models.CharField(max_length=45)
+
+class Hotel(models.Model):
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
+    name = models.CharField(max_length=45)
+    capacity = models.IntegerField() #total cap
+    occupancy = models.IntegerField()
+
+class Location(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    
 
 class Itenary(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
